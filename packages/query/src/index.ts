f59a776c3bc6e18ac81e089500608229cf0b1123
Kristen Hewell Garrett <kristen@phantom.app>
2025-11-08 07:47:43 -0500

    Add async store

--- packages/query/src/index.ts
+++ packages/query/src/index.ts
@@ -2,5 +2,16 @@ export * from './types.js';
 
 export { QueryClient, QueryResultImpl as QueryResult } from './QueryClient.js';
 export type { QueryContext } from './QueryClient.js';
-export type { QueryStore } from './QueryStore.js';
+export type { QueryStore, CachedQuery } from './QueryStore.js';
+export {
+  SyncQueryStore,
+  MemoryPersistentStore,
+  AsyncQueryStore,
+  valueKeyFor,
+  refCountKeyFor,
+  refIdsKeyFor,
+  updatedAtKeyFor,
+  queueKeyFor,
+} from './QueryStore.js';
+export type { SyncPersistentStore, AsyncPersistentStore, AsyncQueryStoreConfig, StoreMessage } from './QueryStore.js';
 export { query, infiniteQuery } from './query.js';
