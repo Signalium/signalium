d9c5d638375947f8620ded4b577aec0c5933a30a
Kristen Hewell Garrett <kristen@phantom.app>
2026-04-06 14:33:26 -0400

    Remove Fetchium

--- docs-fetchium/images.d.ts
+++
@@ -1,9 +0,0 @@
-declare module '@/images/blur-cyan.png' {
-  const src: string;
-  export default src;
-}
-
-declare module '@/images/blur-indigo.png' {
-  const src: string;
-  export default src;
-}
